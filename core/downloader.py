bcfd5bd96c78545880a728ae2780ba252c9aa3c4
aarishpanda10 <aarishpanda10@gmail.com>
2026-07-10 20:34:49 +0530

    Update downloader.py

--- core/downloader.py
+++ core/downloader.py
@@ -1,1 +1,25 @@
+import requests
+from pathlib import Path
 
+
+class Downloader:
+
+    def __init__(self):
+        self.cache = Path("cache")
+        self.cache.mkdir(exist_ok=True)
+
+    def download(self, url, filename):
+
+        file = self.cache / filename
+
+        try:
+            r = requests.get(url, timeout=30)
+
+            if r.status_code == 200:
+                file.write_bytes(r.content)
+                return file
+
+        except Exception as e:
+            print(e)
+
+        return None
